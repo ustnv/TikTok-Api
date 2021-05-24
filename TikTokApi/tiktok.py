7892f82d927ca81610cf2275a56183f437549bf9
ustnv <ustinovichsergey@gmail.com>
2021-05-24 14:37:40 +0300

    fix proxy https

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -1638,7 +1638,7 @@ class TikTokApi:
         if proxy is None and self.proxy is not None:
             proxy = self.proxy
         if proxy is not None:
-            return {"http": proxy}
+            return {"http": proxy, "https": proxy}
         else:
             return None
 
