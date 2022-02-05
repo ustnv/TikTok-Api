2ac9e6d787ad7be648f73d9dde34a7b043a5c404
Ustinov Sergey <ustnv@users.noreply.github.com>
2022-02-05 14:11:29 +0300

    fix

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -1750,8 +1750,6 @@ class TikTokApi:
         if proxy is None and self.proxy is not None:
             proxy = self.proxy
         if proxy is not None:
-            if "foxy" in proxy:
-                return {"http": proxy}
             return {"http": proxy, "https": proxy}
         else:
             return None
