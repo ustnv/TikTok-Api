629fae30e74b4be47278f2c934e8ba26c130e250
Ustinov Sergey <ustnv@users.noreply.github.com>
2022-03-03 11:33:01 +0200

    fix

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -267,12 +267,20 @@ class TikTokApi:
 
         query = {"verifyFp": verify_fp, "_signature": signature}
         url = "{}&{}".format(kwargs["url"], urlencode(query))
+        static_url = "https://m.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F98.0.4758.102%20Safari%2F537.36%20Edg%2F98.0.1108.62&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=true&from_page=user&history_len=2&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=&region=RO&root_referer&screen_height=1080&screen_width=1920&tz_name=Europe%2FBucharest&verifyFp=verify_dca8729afe5c502257ed30b0b070dbdb&webcast_language=en&msToken=ke4xktLgsxyAhsw7mGoAII-6L3--7SZyUDxD7M1_-TNM_2bSagox_yzIeYOpoBC-kPlNMdT-9trNOv3X7q4iLqrHyHY8_p5cheQ-GCrOGnAPjgrgb6bNGgX1H03HwC5q-mZXBzgQ&X-Bogus=DFSzswVLOezANCYfS5qBc2XyYJA4&_signature=_02B4Z6wo00001WyqqDQAAIDATwuGmbdoOXlsqqyAADk7e9"
 
         done = False
         n = 1
         while not done:
             try:
-
+                r = requests.get(static_url, headers={
+                    "method": "GET",
+                    "accept-encoding": "gzip, deflate",
+                    "Referer": referrer,
+                    "user-agent": userAgent,
+                    "x-tt-params": tt_params,
+                }, proxies=self.__format_proxy(proxy), timeout=5)
+                """
                 h = requests.head(
                     url,
                     headers={"x-secsdk-csrf-version": "1.2.5", "x-secsdk-csrf-request": "1"},
@@ -309,6 +317,7 @@ class TikTokApi:
                     timeout=5,
                     **self.requests_extra_kwargs,
                 )
+                """
             except Exception as e:
                 r = None
                 if not (n % 10):
